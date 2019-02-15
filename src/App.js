bf9235908525b716da1b9124ce92532f8171333a
Smbat <sampogosyan1995@gmail.com>
2019-02-15 23:47:00 +0400

    sdfsdf

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import FireManager from "./firebase/FireManager";
-import Students from './components/students'
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Students from "./components/students";
+import Home from "./components/home";
+import Works from "./components/works";
+import Header from "./components/header";
+import { Switch } from "react-router-dom";
+import "./App.css";
+import { ThemeContext } from "./components/contexts";
+import SignIn from "./components/authentication/signin";
+import SignUp from "./components/authentication/signup";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
-  state = {
-    userName: '',
-      age: 0
-  };
-
-  componentDidMount(){
-    FireManager.getStudent('awhmXpzGdG5I1PkFFyl1').then(user => {
-      this.setState({username: user.name, age: user.age})
-    })
+    this.state = {
+      userName: "",
+      age: 0,
+      colorContext: {
+        color: "primary",
+        changeColor: this.changeColor
+      }
+    };
   }
 
-  render() {
-    const {username, age} = this.state;
+  changeColor = () => {
+    const {colorContext} = this.state;
+    const newColor = colorContext.color === 'primary' ? 'secondary' : 'primary';
+    this.setState({
+        colorContext: {
+          ...colorContext,
+            color: newColor
+        }
+    });
+  };
+
+
+    render() {
+    const { colorContext } = this.state;
     return (
+      <>
         <Router>
-          <>
-            <Route exact path="/" render={()=> <p>Invalid Rout</p>}/>
-            <Route path="/students" component={Students}/>
-          </>
+          <div>
+            <ThemeContext.Provider value={colorContext}>
+              <Header />
+
+            <Switch>
+              <Route path="/students" component={Students} />
+              <ThemeContext.Provider value={colorContext}>
+                  <Route path="/home" component={Home} />
+                  <Route path="/works" component={Works} />
+                  <Route path="/signin" component={SignIn} />
+                  <Route path="/signup" component={SignUp} />
+              </ThemeContext.Provider>
+              <Route path="/" component={Home} />
+            </Switch>
+            </ThemeContext.Provider>
+
+          </div>
         </Router>
+      </>
     );
   }
 }
---
+++ src/components/header/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { ThemeContext } from "../contexts";
+import "../../App.css";
+import Switches from "../buttons";
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar }  from 'react-bootstrap/';
+import { Nav }  from 'react-bootstrap/';
+import { FormControl }  from 'react-bootstrap/';
+import { Form }  from 'react-bootstrap/'
+
+
+
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
+
+function Header(props) {
+  const { classes } = props;
+  return (
+
+    <div className={classes.root}>
+        <ThemeContext.Consumer>
+            {({ color, changeColor }) => {
+        return <Navbar bg={ color } variant="dark">
+            <Navbar.Brand href="/home">Student-App</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Toolbar>
+                   <Nav.Link> <Link to="/home" activeClassName="active">
+                       <Button variant="contained" color={ color } className={classes.button}>
+                           Home
+                       </Button>
+                    </Link>
+                   </Nav.Link>
+                    <Nav.Link> <Link to="/students" activeClassName="active">
+                        <Button variant="contained" color={ color } className={classes.button}>
+                            Students
+                        </Button>
+                    </Link>
+                    </Nav.Link>
+                    <Nav.Link> <Link to="/works" activeClassName="active">
+                        <Button variant="contained" color={ color } className={classes.button}>
+                            Works
+                        </Button>
+                    </Link>
+                    </Nav.Link>
+                    <Typography variant="h6" color="inherit" className={classes.grow} />
+                    <Nav.Link> <Link to="/signin" activeClassName="active">
+                        <Button variant="contained" color={ color } className={classes.button}>
+                            Login
+                        </Button>
+                    </Link>
+                    </Nav.Link>
+                </Toolbar>
+            </Nav>
+
+                    Change Theme <Switches onClick={changeColor} />
+
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-info">Search</Button>
+            </Form>
+        </Navbar>
+            }}
+        </ThemeContext.Consumer>
+    </div>
+  );
+}
+
+export default withStyles(styles)(Header);
